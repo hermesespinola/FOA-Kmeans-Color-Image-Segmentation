0fc4db2d1eb853859ffb84e321bc02571d33b9c7
Alberto Villagómez <albertus1301@hotmail.com>
2016-09-29 12:07:48 -0500

    Add files via upload

--- FSOClustering/ForestApplied.m
+++ FSOClustering/ForestApplied.m
@@ -4,17 +4,15 @@ close all;
 
 im = double(imread('icon.png'));
 
-%arr = transpose(squeeze(im(1,:,:)));
-
 CostFunction = @(x, image) kmeans(x, image);  % Cost Function
 
 maxIterations = 6;      %Stopping condition
-minValue = 0;         %Lower limit of the space problem.
-maxValue = 255;          %Upper limit of the space problem.
-minLocalValue = -5;   %Lower limit for local seeding.
-maxLocalValue = 5;    %Upper limit for local seeding.
+minValue = 0;           %Lower limit of the space problem.
+maxValue = 255;         %Upper limit of the space problem.
+minLocalValue = -5;     %Lower limit for local seeding.
+maxLocalValue = 5;      %Upper limit for local seeding.
 initialTrees = 30;      %Initial number of trees in the forest.
-nVar = 3;               %Number of clusters
+nVar = 15;              %Number of clusters
 lifeTime = 6;           %Limit age to be part of the candidate list.
 LSC = 3;                %Local seeding: Number of seeds by tree. MUST BE: LSC <= nVar.
 areaLimit = 100;        %Limit of trees in the forest.
@@ -38,7 +36,6 @@ tree(:, :, 3) = [zeros(initialTrees, 1) round(minValue + rand(initialTrees, nVar
 %2. Main loop
 for i=1:maxIterations
       %2.1 Local seeding
-      
       initialTrees = size(tree, 1);
       for j=1:initialTrees
          if tree(j, 1, 1) == 0     %2.1 If is a new tree
@@ -64,23 +61,18 @@ for i=1:maxIterations
          tree(j, 1, 1) = tree(j, 1, 1) + 1;
       end
       
-      
-      
       %2.2 Population limiting
       for j=1:size(tree, 1)
          %2.2.1 Remove trees with age bigger than life time and add them to candidate list
-         if j > size(tree, 1)   %Para que no cause error al disminuir tree drasticamente
+         if j > size(tree, 1)
             break;
          end
          if tree(j, 1, 1) > lifeTime
              sizeCandidateList = size(candidateList, 1)+1;
-             %candidateList = vertcat(candidateList, tree(j, :, :));
              candidateList(sizeCandidateList, :, :) = tree(j, :, :);
              tree(j, :, :) = [];
          end
       end
-      
-%       tree
       
       %2.2.2 Sort tree according to fitness
       for j=1:size(tree, 1)
@@ -90,37 +82,28 @@ for i=1:maxIterations
       end
       
       %Sorting
-      z=tree(:,nVar+2,:);
-      [values,indices] = sort(z,1) ;
-      B=zeros(size(tree,1),size(tree,2),size(tree,3));
-      for j=1:size(tree,3)
-        B(:,:,j) =tree(indices(:,:,j),:,j);
-      end
-      tree = B;
+      tree(:, :, 1) = sortrows(tree(:, :, 1), nVar+2);
+      tree(:, :, 2) = sortrows(tree(:, :, 2), nVar+2);
+      tree(:, :, 3) = sortrows(tree(:, :, 3), nVar+2);
       %End sorting
-      
       
       
       %2.2.3 Remove tree that exceed area limit and add them to candidate list
       if size(tree, 1) > areaLimit
-         %candidateList(size(tree, 1)+1:size(candidateList)+size(tree, 1)-areaLimit, :, :) = tree(areaLimit+1:size(tree, 1), :, :);
-        %candidateList = vertcat(candidateList(size(tree, 1)+1:size(candidateList)+size(tree, 1)-areaLimit, :, :), tree(areaLimit+1:size(tree, 1), :, :));
-         candidateList = vertcat(candidateList, tree(areaLimit+1:size(tree, 1), :, :));
+         candidateList(size(candidateList, 1)+1:size(candidateList, 1)+size(tree, 1)-areaLimit, :, :) = tree(areaLimit+1:size(tree, 1), :, :);
           tree(areaLimit+1:size(tree, 1), :, :) = [];
       end
       
-%       candidateList
-%         tree
-        
       
       %2.3 Global seeding
       %2.3.1 Choose number of trees from candidate tree
-      selectedTrees = round(transferRate * size(candidateList, 1));
+      selectedTrees = floor(transferRate * size(candidateList, 1));
+      globalParents = randperm(selectedTrees);
       
       %2.3.1 Create new trees
       for j=1:selectedTrees
           sizeTree = size(tree, 1)+1;
-          newTree = tree(j, :, :);
+          newTree = candidateList(globalParents(1, j), :, :);
           newTree(1, 1, 1) = 0;
           for k=1:GSC
               %Solo modifico el componente R
@@ -131,22 +114,24 @@ for i=1:maxIterations
           tree( sizeTree, :, : ) = newTree;
       end
       
+      %Limiting candidateList
+      if size(candidateList, 1) > 10/transferRate
+          candidateList(10/transferRate+1:size(candidateList), :, :) = [];
+      end
       
       %2.4 Update best tree
       tree(1, 1, 1) = 0;
       bestTreeByIteration(i) = tree(1, nVar+2, 1);
       
-      
 end
 
-bestTreeByIteration(1)
-bestTreeByIteration(maxIterations)
+disp(bestTreeByIteration(1));
+disp(bestTreeByIteration(maxIterations));
 
 %Show info
 figure;
 semilogy(bestTreeByIteration, 'LineWidth', 2);
+title 'Forest optimization algorithm for image clustering';
 xlabel('Iteration');
 ylabel('BestTreeCost');
 grid on;
-
-
